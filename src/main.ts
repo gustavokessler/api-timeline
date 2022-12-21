409c81597b3ca57a17f84a71e26995e01f4d5689
gustavokessler <37279803+gustavokessler@users.noreply.github.com>
2022-12-21 14:58:08 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);  
+  
+  app.enableCors();
+  
   await app.listen(3003);
+
+
 }
 bootstrap();
