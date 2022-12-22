65ac5ce5102d96513bab57b6fcbc63bb447db6a0
gustavokessler <37279803+gustavokessler@users.noreply.github.com>
2022-12-22 14:12:35 -0300

    add game resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { ConfigModule } from './config/config.module';
 import { DeckModule } from './deck/deck.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [AuthModule, CardsModule, ConfigModule, DeckModule],
+  imports: [AuthModule, CardsModule, ConfigModule, DeckModule, GameModule],
   controllers: [AppController],
   providers: [AppService],
 })
