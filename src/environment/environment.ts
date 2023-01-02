92f3bdc5395b2fa12d1ae31d9eaef58713b8f9bf
gustavokessler <gustavo.kessler@hotmail.com>
2023-01-02 11:32:17 -0300

    external dataBase

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,11 +2,11 @@ import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOpti
 
 export const ormConfigFactory: () => MysqlConnectionOptions = () => ({
   type: "mysql",
-  host: "localhost",
+  host: "sql.freedb.tech",
   port: 3306,
-  username: "root", // TODO DEPOIS MUDAR ISSO AQUI PARA O TIMELINE
-  password: "neshapass",
-  database: "timeline",
+  username: "freedb_api_user", // TODO DEPOIS MUDAR ISSO AQUI PARA O TIMELINE
+  password: "@R@Ufz9EA&GV8Wc",
+  database: "freedb_timeline",
   entities: ["dist/**/*.entity{.ts,.js}"],
   autoLoadEntities: true,
   isGenerated: true,
