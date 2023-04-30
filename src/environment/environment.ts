23ca6047037e48f78ad0b304cc3d28d3fe90c6d9
gustavokessler <gustavo.kessler@hotmail.com>
2023-04-30 00:17:39 -0300

    env fix

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,13 +2,13 @@ import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOpti
 
 export const ormConfigFactory: () => MysqlConnectionOptions = () => ({
   type: "mysql",
-  host: "sql.freedb.tech",
+  host: "127.0.0.0",
   port: 3306,
-  username: "freedb_api_user", // TODO DEPOIS MUDAR ISSO AQUI PARA O TIMELINE
-  password: process.env.DATABASE_PASSWORD || "neshapass",
-  database: "freedb_timeline",
+  username: "root", // TODO DEPOIS MUDAR ISSO AQUI PARA O TIMELINE
+  password: "neshapass",
+  database: "timeline",
   entities: ["dist/**/*.entity{.ts,.js}"],
   autoLoadEntities: true,
   isGenerated: true,
-  generationStrategy: 'increment',
+  generationStrategy: "increment",
 });
